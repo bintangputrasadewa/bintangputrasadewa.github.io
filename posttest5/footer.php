9765376a3c9e5f94fb0e9284c1d06c6e6de0ade0
Sadewa <145417735+bintangputrasadewa@users.noreply.github.com>
2023-10-19 22:22:46 +0800

    Delete posttest5 directory
    Forget to add SQL folder database

--- posttest5/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-		<div class="container">
-			<small>Copyright &copy; 2023 - Bintang Putra Sadewa x JAMBRED STORE .</small>
-		</div>
-</footer>
--- posttest5/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $response = "Terima kasih, $name! Pesan Anda telah diterima.";
-  echo $response;
-}
-?>
